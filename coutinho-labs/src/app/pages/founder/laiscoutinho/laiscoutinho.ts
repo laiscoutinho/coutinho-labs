5fe474e8d0e033f8809528b515b2656e8b8764f5
Laís Coutinho <laiscc575@gmail.com>
2026-05-01 20:08:45 -0300

    feat: Adding a page of founder

---
+++ coutinho-labs/src/app/pages/founder/laiscoutinho/laiscoutinho.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laiscoutinho',
+  imports: [],
+  templateUrl: './laiscoutinho.html',
+  styleUrl: './laiscoutinho.scss',
+})
+export class Laiscoutinho {}
