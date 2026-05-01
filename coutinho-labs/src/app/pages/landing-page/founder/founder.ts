bcf97155b81b2e9a912db9b9c929e0e3043f073a
Laís Coutinho <laiscc575@gmail.com>
2026-05-01 20:06:47 -0300

    feat: Adding a landing page - section founder

---
+++ coutinho-labs/src/app/pages/landing-page/founder/founder.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-founder',
+  imports: [],
+  templateUrl: './founder.html',
+  styleUrl: './founder.scss',
+})
+export class Founder {}
