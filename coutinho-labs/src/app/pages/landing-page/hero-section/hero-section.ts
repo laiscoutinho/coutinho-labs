b538f168e0656bea28ee29d0b53a9231d5a1ba3c
Laís Coutinho <laiscc575@gmail.com>
2026-05-01 20:07:23 -0300

    feat: Adding a landing page - section hero

--- coutinho-labs/src/app/pages/landing-page/hero-section/hero-section.ts
+++ coutinho-labs/src/app/pages/landing-page/hero-section/hero-section.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../components/btns/button-component/button-component';
+
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
-export class HeroSection {}
+
+export class HeroSection {
+
+
+
+}
