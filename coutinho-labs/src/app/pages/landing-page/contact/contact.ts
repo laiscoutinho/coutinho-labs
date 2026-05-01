24661dd6fbd09c694ebc9ead3ba34594a0c23d05
Laís Coutinho <laiscc575@gmail.com>
2026-05-01 20:06:34 -0300

    feat: Adding a landing page - section contact

---
+++ coutinho-labs/src/app/pages/landing-page/contact/contact.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../components/btns/button-component/button-component';
+
+@Component({
+  selector: 'app-contact',
+  imports: [ButtonComponent],
+  templateUrl: './contact.html',
+  styleUrl: './contact.scss',
+})
+export class Contact {}
